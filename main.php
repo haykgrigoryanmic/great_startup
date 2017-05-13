1203f6ab0b9e002a2790df34ba5c453c8d32b478
Armine <test@test.com>
2017-05-13 16:46:05 +0400

    Get.php added, main and student phps updated

--- main.php
+++ main.php
@@ -1,1 +1,7 @@
 <h3>Main page</h3>
+
+<form action="get.php" method="get">
+    Name: <input type="text" name="name"><br><br>
+    E-mail: <input type="text" name="email"><br><br>
+    <input type="submit">
+</form>
--- students.php
+++ students.php
@@ -1,1 +1,8 @@
 <h3>Students page</h3>
+
+<?php
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+
+?>
