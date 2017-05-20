eca5c977357b2fda674b7812d04a441c88120304
Armine <test@test.com>
2017-05-20 20:39:48 +0400

    database name change

--- resources/gstartup-armine.sql
+++ resources/gstartup-armine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2017 at 06:17 PM
+-- Generation Time: May 20, 2017 at 06:38 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gstartup`
+-- Database: `gstartup-armine`
 --
 
 -- --------------------------------------------------------
