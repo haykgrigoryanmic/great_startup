e41056032d03747c5deeb907ed24705cabdf92c5
Hayk Grigoryan <hayk.g.grigoryan@gmail.com>
2017-05-20 22:43:45 +0400

    new nav bar

--- application/views/nav_bar.php
+++ application/views/nav_bar.php
@@ -1,22 +1,48 @@
-<div class="well">
-    <h1 id="color">Welcome to our <span class="red">GREAT</span> startup page</h1>
-</div>
-<div class="text">Text 1</div>
-<div class="text">Text 2</div>
-<p class="text">Text 3</p>
-<p class="text">Text 4</p>
-<p id="demo"></p>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo site_url('') ?>">Main brand</a>
+        </div>
 
-<div id="navbar">
-    <ul>
-        <li>
-            <a href="<?php echo site_url('users') ?>">User page</a>
-        </li>
-        <li>
-            <a href="<?php echo site_url('sights') ?>">Sightseeing</a>
-        </li>
-        <li>
-            <a href="<?php echo site_url('') ?>">About us</a>
-        </li>
-    </ul>
-</div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="<?php echo site_url('users') ?>">User page</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo site_url('sights') ?>">Sightseeing</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo site_url('') ?>">About us</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#">Sign Up</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
