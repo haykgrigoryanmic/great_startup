2f6d1b185e39f17d0794320c24aa50653acaa28d
Hayk Grigoryan <hayk.g.grigoryan@gmail.com>
2017-05-20 22:07:35 +0400

    Boortrap and jquery

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,9 +1,3 @@
-<script src="<?php echo base_url('/js/jquery-3.2.1.js');?>"></script>
-<script src="<?php echo base_url('/js/bootstrap.js');?>"></script>
-<script src="<?php echo base_url('/js/sample.js');?>"></script>
-
-
-
-
+<script type="text/javascript" src="<?php echo base_url();?>public/js/script.js"></script>
 </body>
 </html>
--- application/views/head.php
+++ application/views/head.php
@@ -3,21 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-    <link rel="stylesheet" href="<?php echo site_url()?>;/css/bootstrap.css)">
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
 
-    <?php echo link_tag('css/bootstrap.css')
-    ?>
-    <?php echo link_tag('css/style.css')
-    ?>
-
-    <style>
-
-        body{
-            background-color: #2e8ece;
-        }
-    </style>
-
+    <link rel="stylesheet" href="<?php echo base_url();?>public/css/bootstrap.css" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>public/css/style.css" type="text/css" media="screen"/>
+    <script type="text/javascript" src="<?php echo base_url();?>public/js/jquery-3.1.0.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>public/js/bootstrap.js"></script>
 </head>
 <body>
